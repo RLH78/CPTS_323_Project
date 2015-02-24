6747761d15d86a4e9cde6523aeac0c0956f1a96e
RLH78 <.>
2015-02-24 15:28:41 -0800

    SadCL

---
+++ Production/Src/Applications/SadCL/SadCL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAD.core;
+
+namespace SadCL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Var launcher = new SAD.core.Devices.MissleLauncher();
+            Namespace SAD.core.Devices.MissileLauncher();
+        }
+    }
+}
