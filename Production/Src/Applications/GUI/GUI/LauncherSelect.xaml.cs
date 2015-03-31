5bf0630c8ddde0d71304862d6d3ecdcca86b078f
RLH78 <.>
2015-03-30 20:44:55 -0700

    Moved launcherSelect stuff and installed ookii

--- Production/Src/Applications/GUI/GUI/LauncherSelect.xaml.cs
+++ Production/Src/Applications/GUI/GUI/LauncherSelect.xaml.cs
@@ -24,41 +24,6 @@ namespace GUI
         public LauncherSelect()
         {
             InitializeComponent();
-            setToMock = 0;
-            setToDC = 1;
-        }
-
-        private int launcherType { get; set; }
-        public IMissileLauncher aLauncher { get; private set; }
-        private int setToMock;
-        private int setToDC;
-
-        private void Mock_Launcher_Checked(object sender, RoutedEventArgs e)
-        {
-            MissileLauncherFactory factory = new MissileLauncherFactory();
-            aLauncher = factory.createMissileLauncher(SAD.core.Factories.launcherType.mock);
-            if (Mock_Launcher.IsChecked == true)
-            {
-                MessageBox.Show("Mock Launcher created");
-                launcherType = setToMock;
-                MainWindow win2 = new MainWindow();
-                win2.Show();
-                this.Close();
-            }
-        }
-
-        private void Dream_Cheeky_Checked(object sender, RoutedEventArgs e)
-        {
-            MissileLauncherFactory factory = new MissileLauncherFactory();
-            aLauncher = factory.createMissileLauncher(SAD.core.Factories.launcherType.dreamC);
-            if (Dream_Cheeky.IsChecked == true)
-            {
-                MessageBox.Show("DreamCheeky created");
-                launcherType = setToDC;
-                MainWindow win2 = new MainWindow();
-                win2.Show();
-                this.Close();
-            }
         }
     }
 }
--- Production/Src/Applications/GUI/GUI/launcherViewModel.cs
+++ Production/Src/Applications/GUI/GUI/launcherViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using SAD.core.Factories;
 using SAD.core.Devices;
+using System.Windows;
 
 namespace GUI
 {
@@ -16,17 +17,128 @@ namespace GUI
             //
         }
 
-        myServerMessage createDC;
-        myServerMessage createMock;
+        myDCCreator createDC;
+        myMockCreator createMock;
+        public IMissileLauncher launcher_view_Launcher { get; private set; }
 
-        public ICommand _create_DC
+        public ICommand _create_DC_Command
         {
-            get;
-            set;
+            get
+            {
+                if (createDC == null)
+                {
+                    createDC = new myDCCreator(param => DreamCheekyCreate());
+                }
+                return createDC;
+            }
+        }
+        public ICommand _create_Mock_Command
+        {
+            get
+            {
+                if (createMock == null)
+                {
+                    createMock = new myMockCreator(param => MockCreate());
+                }
+                return createMock;
+            }
+        }
+        public void DreamCheekyCreate()
+        {
+            MissileLauncherFactory factory = new MissileLauncherFactory();
+            launcher_view_Launcher = factory.createMissileLauncher(SAD.core.Factories.launcherType.dreamC);
+            MessageBox.Show("DreamCheeky created YAY");
+            MainWindow win2 = new MainWindow();            
+            Window win = Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "LauncherSelectName");
+            win.Close();  
+            win2.Show(); 
+        }  
+
+        public void MockCreate()
+        {
+            MissileLauncherFactory factory = new MissileLauncherFactory();
+            launcher_view_Launcher = factory.createMissileLauncher(SAD.core.Factories.launcherType.mock);
+            MessageBox.Show("Mock Launcher created");
+            MainWindow win2 = new MainWindow();
+            Window win = Application.Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "LauncherSelectName");
+            win.Close();
+            win2.Show();
+        }
+    }
+
+    public class myDCCreator : ICommand
+    {
+        readonly Action<object> _ActionToExecute;
+        readonly Predicate<object> _ActionCanExecute;
+        public myDCCreator(Action<object> inActionToExecute)
+            : this(inActionToExecute, null)
+        {
+            // m_model = model;
+        }
+
+        public myDCCreator(Action<object> inActionToExecute, Predicate<object> inActionCanExecute)
+        {
+            if (inActionToExecute == null)
+                throw new ArgumentNullException("execute");
+
+            _ActionToExecute = inActionToExecute;
+            _ActionCanExecute = inActionCanExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            // throw new NotImplementedException();
+            return _ActionCanExecute == null ? true : _ActionCanExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            // throw new NotImplementedException();
+            _ActionToExecute(parameter);
         }
     }
 
 
+    public class myMockCreator : ICommand
+    {
+        readonly Action<object> _ActionToExecute;
+        readonly Predicate<object> _ActionCanExecute;
+        public myMockCreator(Action<object> inActionToExecute)
+            : this(inActionToExecute, null)
+        {
+            // m_model = model;
+        }
 
+        public myMockCreator(Action<object> inActionToExecute, Predicate<object> inActionCanExecute)
+        {
+            if (inActionToExecute == null)
+                throw new ArgumentNullException("execute");
+
+            _ActionToExecute = inActionToExecute;
+            _ActionCanExecute = inActionCanExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _ActionCanExecute == null ? true : _ActionCanExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _ActionToExecute(parameter);
+        }
+    }
    
 }
